6cb863b83fb262a51dc0a230a2301ad30d0510f4
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-28 23:58:14 +0800

    test: define HyperOS 8 shared title size semantics

--- library/libhook/src/test/java/com/sevtinge/hyperceiler/libhook/rules/home/title/TitleFontSizeNativeBridgeTest.java
+++ library/libhook/src/test/java/com/sevtinge/hyperceiler/libhook/rules/home/title/TitleFontSizeNativeBridgeTest.java
@@ -1,5 +1,6 @@
 package com.sevtinge.hyperceiler.libhook.rules.home.title;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -14,5 +15,21 @@ public class TitleFontSizeNativeBridgeTest {
         assertFalse(TitleFontSizeNativeBridge.supportsLauncherVersion(801025466));
         assertFalse(TitleFontSizeNativeBridge.supportsLauncherVersion(800000000));
         assertFalse(TitleFontSizeNativeBridge.supportsLauncherVersion(899999999));
+    }
+
+    @Test
+    public void sharedNativeGetterRequiresSameNonDefaultDesktopAndDrawerSize() {
+        assertFalse(TitleFontSizeNativeBridge.canUseSharedSize(12, 12));
+        assertFalse(TitleFontSizeNativeBridge.canUseSharedSize(14, 12));
+        assertFalse(TitleFontSizeNativeBridge.canUseSharedSize(12, 14));
+        assertFalse(TitleFontSizeNativeBridge.canUseSharedSize(14, 16));
+        assertTrue(TitleFontSizeNativeBridge.canUseSharedSize(14, 14));
+    }
+
+    @Test
+    public void nativeScalePreservesLauncherPixelsRelativeToLegacyTwelveSpDefault() {
+        assertEquals(1.0, TitleFontSizeNativeBridge.scaleMultiplierFor(12), 0.000001);
+        assertEquals(1.25, TitleFontSizeNativeBridge.scaleMultiplierFor(15), 0.000001);
+        assertEquals(0.75, TitleFontSizeNativeBridge.scaleMultiplierFor(9), 0.000001);
     }
 }
