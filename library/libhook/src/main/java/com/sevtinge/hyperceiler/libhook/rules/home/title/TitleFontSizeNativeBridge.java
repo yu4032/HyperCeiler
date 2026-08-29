015bde4808345363a4420fb11290e10055a3ec77
yu4032 <4032542465@smail.lnu.edu.cn>
2026-08-29 08:07:06 +0800

    fix: pin HyperOS 8 title size AOT getter

--- library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/home/title/TitleFontSizeNativeBridge.java
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/home/title/TitleFontSizeNativeBridge.java
@@ -12,6 +12,14 @@ package com.sevtinge.hyperceiler.libhook.rules.home.title;
 final class TitleFontSizeNativeBridge {
     private static final int SUPPORTED_LAUNCHER_VERSION = 801025465;
     private static final int LEGACY_DEFAULT_SP = 12;
+    private static final long TARGET_OFFSET = 0x101E71CL;
+    private static final byte[] TARGET_FINGERPRINT = new byte[] {
+        (byte) 0xfd, 0x79, (byte) 0xbf, (byte) 0xa9, (byte) 0xfd, 0x03, 0x0f, (byte) 0xaa,
+        (byte) 0xe0, 0x03, 0x01, (byte) 0xaa, 0x38, 0x63, (byte) 0xe8, (byte) 0x97,
+        0x01, (byte) 0xf0, 0x46, (byte) 0xb8, 0x21, (byte) 0x80, 0x1c, (byte) 0x8b,
+        0x20, (byte) 0xf0, 0x41, (byte) 0xfc, (byte) 0xef, 0x03, 0x1d, (byte) 0xaa,
+        (byte) 0xfd, 0x79, (byte) 0xc1, (byte) 0xa8, (byte) 0xc0, 0x03, 0x5f, (byte) 0xd6
+    };
 
     private TitleFontSizeNativeBridge() {
     }
@@ -26,5 +34,13 @@ final class TitleFontSizeNativeBridge {
 
     static double scaleMultiplierFor(int requestedSp) {
         return requestedSp / (double) LEGACY_DEFAULT_SP;
+    }
+
+    static long targetOffset() {
+        return TARGET_OFFSET;
+    }
+
+    static byte[] targetFingerprint() {
+        return TARGET_FINGERPRINT.clone();
     }
 }
